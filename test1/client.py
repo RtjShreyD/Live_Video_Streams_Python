6f615d77ff5c0906befc409a291b90bf1cc86d3a
RtjShreyD <shreyanshece1041@gmail.com>
2020-04-22 14:59:12 +0530

    optimized

--- test1/client.py
+++ test1/client.py
@@ -3,11 +3,19 @@ import numpy as np
 import socket
 import sys
 import pickle
-import struct ### new code
+import struct
+
 cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('localhost',8089))
+
 while True:
     ret,frame=cap.read()
-    data = pickle.dumps(frame) ### new code
-    clientsocket.sendall(struct.pack("H", len(data))+data) ### new code
+    # Serialize frame
+    data = pickle.dumps(frame)
+
+    # Send message length first
+    message_size = struct.pack("L", len(data)) ### CHANGED
+
+    # Then data
+    clientsocket.sendall(message_size + data)
