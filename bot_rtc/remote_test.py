47e33026166b7f01ecfee4bca2c8493723787827
RtjShreyD <shreyanshece1041@gmail.com>
2020-05-12 15:23:38 +0530

    Tested on sever, Multi-client, Single-Client, Worksgit add bot_rtc/git add bot_rtc/

---
+++ bot_rtc/remote_test.py
@@ -0,0 +1,24 @@
+import asyncio
+from rtcbot import Websocket, RTCConnection, CVCamera
+
+cam = CVCamera()
+conn = RTCConnection()
+conn.video.putSubscription(cam)
+
+async def connect():
+    ws = Websocket("http://localhost:8080/xyz")
+    #ws = Websocket("http://13.233.21.7:8080/node2")
+    remoteDescription = await ws.get()
+    # print(remoteDescription)
+    robotDescription = await conn.getLocalDescription(remoteDescription)
+    #print(robotDescription)
+    ws.put_nowait(robotDescription)
+    print("Started WebRTC")
+    await ws.close()
+
+asyncio.ensure_future(connect())
+try:
+    asyncio.get_event_loop().run_forever()
+finally:
+    cam.close()
+    conn.close()
